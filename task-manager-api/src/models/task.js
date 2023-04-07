e1ed87e82c6e62078fad1075680a741053e99d57
Ma <jianq@northeastern.edu>
2023-04-07 14:17:03 -0700

    Implemented Tweeter backend

---
+++ task-manager-api/src/models/task.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const taskSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    image: {
+        type: Buffer,
+        required: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+})
+
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = Task
---
+++ task-manager-api/src/routers/task.js
@@ -0,0 +1,171 @@
+const express = require('express')
+const Task = require('../models/task')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+const multer = require('multer')
+const sharp = require('sharp')
+
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks/:userId', async (req, res) => {
+    const sort = {}
+    const match = { owner: req.params.userId }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+    try {
+        await Task.find(match)
+          .limit(parseInt(req.query.limit))
+          .skip(parseInt(req.query.skip))
+          .sort(sort)
+          .exec((error, tasks) => {
+            if (error) {
+              res.status(500).send();
+            } else {
+              res.send(tasks);
+            }
+          });
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
+    try {
+        await Task.find(match)
+          .limit(parseInt(req.query.limit))
+          .skip(parseInt(req.query.skip))
+          .sort(sort)
+          .exec((error, tasks) => {
+            if (error) {
+              res.status(500).send();
+            } else {
+              res.send(tasks);
+            }
+          });
+      } catch (e) {
+        res.status(500).send();
+      }
+})
+
+// router.get('/tasks/:id', auth, async (req, res) => {
+//     const _id = req.params.id
+
+//     try {
+//         const task = await Task.findOne({_id, owner: req.user._id})
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+//         res.send(task)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+       
+        if (!task) {
+            return task.status(404).send()
+        }
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/tasks/:id/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250, fit: 'fill'}).png().toBuffer()
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+       
+        if (!task) {
+            return task.status(404).send()
+        }
+        task["image"] = buffer
+        await task.save()
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id/avatar', auth, async (req, res) => {
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+       
+        if (!task) {
+            return task.status(404).send()
+        }
+        task["image"] = undefined
+        await task.save()
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+module.exports = router
